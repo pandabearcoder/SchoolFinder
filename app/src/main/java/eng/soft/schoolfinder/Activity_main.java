0ad1d90d86486d5bdec63a878a55645db37e96ac
pandabearcoder <pandabearcoder>
2016-02-05 20:51:41 +0800

    Project's Initial Commit

--- app/src/main/java/eng/soft/schoolfinder/Activity_main.java
+++ app/src/main/java/eng/soft/schoolfinder/Activity_main.java
@@ -1,5 +1,7 @@
 package eng.soft.schoolfinder;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,15 +12,48 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import eng.soft.schoolfinder.fragments.Fragment_Home;
+import eng.soft.schoolfinder.fragments.Fragment_about;
+import eng.soft.schoolfinder.fragments.Fragment_finder;
+import eng.soft.schoolfinder.fragments.Fragment_k12;
+import eng.soft.schoolfinder.fragments.Fragment_schools;
+import eng.soft.schoolfinder.fragments.Fragment_tracks;
+
 public class Activity_main extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    //Globally Declare Fragments
+    Fragment_Home home_display;
+    Fragment_k12 k12_display;
+    Fragment_finder finder_display;
+    Fragment_schools schools_display;
+    Fragment_tracks tracks_display;
+    Fragment_about about_display;
+    FragmentManager mFragmentManager;
+
+    Toolbar toolbar;
+
+    public Activity_main() {
+        home_display = new Fragment_Home();
+        k12_display = new Fragment_k12();
+        finder_display = new Fragment_finder();
+        schools_display = new Fragment_schools();
+        tracks_display = new Fragment_tracks();
+        about_display = new Fragment_about();
+        mFragmentManager = getFragmentManager();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Home");
         setSupportActionBar(toolbar);
+
+        FragmentTransaction initTransaction = mFragmentManager.beginTransaction();
+        initTransaction.replace(R.id.frag_container,home_display);
+        initTransaction.commit();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -42,44 +77,47 @@ public class Activity_main extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
+        FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
 
         if (id == R.id.nav_home) {
-
+            mFragmentTransaction.replace(R.id.frag_container,home_display);
+            mFragmentTransaction.commit();
+            toolbar.setTitle("Home");
         } else if (id == R.id.nav_finder) {
-
+            mFragmentTransaction.replace(R.id.frag_container,finder_display);
+            mFragmentTransaction.commit();
+            toolbar.setTitle("Finder");
         } else if (id == R.id.nav_k12) {
-
+            mFragmentTransaction.replace(R.id.frag_container,k12_display);
+            mFragmentTransaction.commit();
+            toolbar.setTitle("The K to 12");
         } else if (id == R.id.nav_schools) {
-
+            mFragmentTransaction.replace(R.id.frag_container,schools_display);
+            mFragmentTransaction.commit();
+            toolbar.setTitle("Schools");
         } else if (id == R.id.nav_tracks) {
-
+            mFragmentTransaction.replace(R.id.frag_container,tracks_display);
+            mFragmentTransaction.commit();
+            toolbar.setTitle("K-12 Tracks");
         } else if (id == R.id.nav_about) {
-
+            mFragmentTransaction.replace(R.id.frag_container,about_display);
+            mFragmentTransaction.commit();
+            toolbar.setTitle("About");
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
